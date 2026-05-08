7e581246420de50133bc6e7cb295729ea98ca6a6
mehidealways <mehidehasanmilon444@.com>
2026-05-09 00:40:27 +0600

    add footer section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-
 import Banner from '@/components/Banner';
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div >
-      <Banner/> 
-
+    <div>
+      <Banner />
     </div>
   );
 }
